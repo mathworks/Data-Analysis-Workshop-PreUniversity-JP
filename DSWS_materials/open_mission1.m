b04b980940bdcdb54237fd6e5bc05c3796a97d9d
Jiro Doke <jiro.doke@gmail.com>
2022-04-16 16:33:53 +0900

    Initial commit

---
+++ DSWS_materials/open_mission1.m
@@ -0,0 +1,9 @@
+%% 環境設定用 MATLAB(r) -スクリプト
+% Copyright 2020 - 2022 The MathWorks, Inc.
+
+% tools と solutions フォルダー内のコマンドが実行できるようにパス設定
+addpath('./tools')
+addpath('./ws_files')
+
+% Mission 1 ファイルを開く
+open('sbda_mission1.mlx')
